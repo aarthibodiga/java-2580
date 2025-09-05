33a860d99bb6ac20ff0e73fdaf8ecdbda33f4a66
aarthibodiga <aarthibodiga456@gmail.com>
2025-09-05 23:11:16 +0530

    assigned variable values

---
+++ variables.java
@@ -0,0 +1,27 @@
+public class variables {
+    public static void main(String[] args) {
+        byte byteNumber = 56;
+        short shortNumber = 105;
+        int intNumber = 25;
+        int longNumber = 25000;
+        float floatNumber = 3.55f;
+        double doubleNumber = 2.15;
+        boolean booleanValue = true;
+        char charValue ='a';
+        String message = " Aarthi";
+        System.out.println(message);
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanValue);
+        System.out.println(charValue);
+        System.out.println(intNumber);
+        
+
+
+
+    }
+    
+}
