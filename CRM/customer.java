9e0caa42955b1d3d4ce3615d5c8f5b0704db75d1
aarthibodiga <aarthibodiga456@gmail.com>
2025-09-07 21:55:54 +0530

    customer

---
+++ CRM/customer.java
@@ -0,0 +1,31 @@
+package CRM ;
+
+public class customer {
+    public static void main(String[]args){
+        byte CustomerID =112;
+        String CustonerName ="Aarthi";
+        String email="Aarthi@gmail.com";
+        long customerMobileNumber= 630157894;
+        char Gender='F';
+        String  CustomerAddress="LB.Nagar,Hyderabad";
+        String CustomerNationality ="INDIAN";
+        int RegistrationDate=24-5-2024;
+        short CustomerTotalspend=2500;
+
+
+        System.out.println("Customer ID:"+CustomerID);
+        System.out.println("Customer Name:"+CustonerName);
+        System.out.println("Customer Email Address:" +email);
+        System.out.println("Customer MobileNumber:"+customerMobileNumber);
+        System.out.println("Customer Gender:" +Gender);
+        System.out.println("Customer Address:"+CustomerAddress);
+        System.out.println("Customer Nationality:"+CustomerNationality);
+        System.out.println("Customer Registration Date:"+RegistrationDate);
+        System.out.println("Customer Total Bill:"+CustomerTotalspend);
+
+
+
+        
+    }
+    
+}
