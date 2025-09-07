6834f7de8e8c0560fa514e28a9f528a0cdfa8ab9
aarthibodiga <aarthibodiga456@gmail.com>
2025-09-07 20:36:16 +0530

    course

---
+++ LMS/courses.java
@@ -0,0 +1,30 @@
+package LMS;
+
+public class courses {
+
+    public static void main(String [] args){
+        int CourseID =2508;
+        String courseName ="Java";
+        String courseInstructor="Ravi";
+        byte courseDurationInMonths = 4;
+        char IsCourseOnline='Y';
+        int coursePrice =20000;
+        float courseDiscount = 2.5f;
+        float courseRating = 4.5f;
+        
+         System.out.println("ID IS:"  +CourseID );
+         System.out.println( "Name is:" +courseName);
+         System.out.println("Instructor name is:" +courseInstructor);
+         System.out.println("Duration:" +courseDurationInMonths);
+         System.out.println("Is Online:" +IsCourseOnline);
+         System.out.println("Price:" +coursePrice);
+         System.out.println( "Discount:" +courseDiscount);
+         System.out.println("Rating:" +courseRating);
+          
+
+
+
+
+    }
+    
+}
