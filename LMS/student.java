ba67a252a4ee4ca2d44402cfaaed28c5f687e9c0
aarthibodiga <aarthibodiga456@gmail.com>
2025-09-07 20:08:11 +0530

    Student

---
+++ LMS/student.java
@@ -0,0 +1,23 @@
+package LMS;
+
+public class student {
+    public static void main(String[] args){
+        int studentID=101;
+        String studentName="Aarthi";
+        byte studentAge=21;
+        char studentGender='F';
+        float studentRating= 4.5f;
+        boolean isInstructor = false;
+        short courseID =2508;
+
+        System.out.println("ID IS:" +studentID);
+        System.out.println("Name is:" +studentName);
+        System.out.println("Age is:" +studentAge);
+        System.out.println("Gender is:"+studentGender);
+        System.out.println("Rating is:" +studentRating);
+        System.out.println("Is Trainer:" +isInstructor);
+        System.out.println("Course ID:" +courseID);
+        
+    }
+    
+}
