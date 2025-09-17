51ba199877c713d4590a8f9b0591f6f6d85fab58
aarthibodiga <aarthibodiga456@gmail.com>
2025-09-17 20:34:20 +0530

    car main

---
+++ CRM/carMain.java
@@ -0,0 +1,10 @@
+package CRM;
+import java.util.Scanner;
+
+public class carMain {
+    public  static void main( String[]args){
+        EmiCalculator car = new EmiCalculator();
+        car.Calculation();
+    }
+    
+}
